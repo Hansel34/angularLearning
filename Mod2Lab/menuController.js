78e92b318c8a6ea8de75fba2d717801558daa4df
hanselqiu@circlecvi.com <hanselqiu@circlecvi.com>
2018-06-29 16:55:54 -0600

    Up to lab3

--- Mod2Lab/menuController.js
+++ Mod2Lab/menuController.js
@@ -1,5 +1,11 @@
 app.controller("menuController",[
 	"$scope",
 	function($scope){
+		$scope.model = {title: 'Our Menu'};
+		$scope.model.mainDish = {name:'',price:''}
+		$scope.changeMainDish = function(item, item2){
+			$scope.model.mainDish.name=item;
+			$scope.model.mainDish.price=item2;
+		}
 	}
 	]);
